8b8687e875de9e5975f8291daf4cec67d10683de
mikeatgl <speakoftrees@gmail.com>
2015-04-26 12:13:57 -0700

    Custom chordBox directive with isolate scope working.

--- public/angular/lantana.js
+++ public/angular/lantana.js
@@ -9,4 +9,27 @@
     };
   });
 
+  app.directive('chordBox', function() {
+  	return {
+  		restrict: 'E',
+  		scope: {},
+  		templateUrl: '../angular/chordBox.html'
+  	}
+
+  });
+
 })();
+
+// angular.module('moduleName')
+//     .directive('myDirective', function () {
+//     return {
+//         restrict: 'EA', //E = element, A = attribute, C = class, M = comment         
+//         scope: {
+//             //@ reads the attribute value, = provides two-way binding, & works with functions
+//             title: '@'         },
+//         template: '<div>{{ myVal }}</div>',
+//         templateUrl: 'mytemplate.html',
+//         controller: controllerFunction, //Embed a custom controller in the directive
+//         link: function ($scope, element, attrs) { } //DOM manipulation
+//     }
+// });
