19eec289c840a965c676abc07b68a98810cf689e
Mike Burnett <speakoftrees@gmail.com>
2015-04-28 20:23:05 -0700

    Add chord directive now working.

--- public/angular/lantana.js
+++ public/angular/lantana.js
@@ -15,21 +15,20 @@
   		scope: {},
   		templateUrl: '../angular/chordBox.html'
   	}
-
   });
 
-})();
+  app.directive('addChord', function($compile) {
+    return {
+      restrict: 'E',
+      templateUrl: '../angular/addChord.html',
+      controller: function($scope, $element) {
+        $scope.add = function() {
+          var el = $compile('<chord-box></chord-box>')($scope);
+          $element.prepend(el);
+        };
+      }
+    };
+  });
 
-// angular.module('moduleName')
-//     .directive('myDirective', function () {
-//     return {
-//         restrict: 'EA', //E = element, A = attribute, C = class, M = comment         
-//         scope: {
-//             //@ reads the attribute value, = provides two-way binding, & works with functions
-//             title: '@'         },
-//         template: '<div>{{ myVal }}</div>',
-//         templateUrl: 'mytemplate.html',
-//         controller: controllerFunction, //Embed a custom controller in the directive
-//         link: function ($scope, element, attrs) { } //DOM manipulation
-//     }
-// });
+
+})();
