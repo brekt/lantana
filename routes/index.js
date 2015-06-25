96e47c2b916c623fed8ba18eec591f49d2454f06
mikeatgl <speakoftrees@gmail.com>
2015-06-24 22:40:04 -0400

    Adding some Node modules and routing.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
 var level = require('level');
 var db = level('./lantanadb');
+var uuid = require('uuid');
+var secretKey = uuid.v4();
+
+
+
+// Routing
+
+router.get('/', function(req, res, next) {
+	res.sendFile('index.html');
+});
+
+router.get('/login', function(req, res, next) {
+	res.sendFile('login.html');
+});
+
+router.get('/signup', function(req, res, next) {
+	res.sendfile('signup.html');
+});
 
 // The code below tested LevelDB.
 
@@ -18,10 +35,5 @@ var db = level('./lantanadb');
 // 	});
 // });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	res.sendFile('index.html');
-  // res.render('index', { title: 'Express' });
-});
 
 module.exports = router;
